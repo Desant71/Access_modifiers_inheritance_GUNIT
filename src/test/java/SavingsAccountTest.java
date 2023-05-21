2b17324e0e95147589f6b0f21fa6fc757bd5c40a
Desant71 <desant31081993@gmail.com>
2023-05-21 14:14:40 +0300

    add all files2

--- src/test/java/SavingsAccountTest.java
+++ src/test/java/SavingsAccountTest.java
@@ -1,15 +1,17 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SavingsAccountTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void pay() {
         SavingsAccount test1 = new SavingsAccount("Netology", 100000, 100);
         boolean operation = test1.pay(99000);
         assertTrue( operation);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void add() {
         SavingsAccount test2 = new SavingsAccount("Nikita", 1000, 200);
         boolean operation2 = test2.add(2000);
